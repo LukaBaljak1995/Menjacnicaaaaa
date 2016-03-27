3881595ba612ea94f704c2612978c86b0dd2c9f7
lukabaljak <luk.abaljak@hotmail.com>
2016-03-27 17:51:26 +0200

    Napravljena klasa Implementacija koja implementira interfejs MenjacnicaInterfejs.

---
+++ Menjacnicaaa/src/Implementacija.java
@@ -0,0 +1,35 @@
+import java.util.GregorianCalendar;
+
+public class Implementacija implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, double srednjiKurs, double prodajniKurs, double kupovniKurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double pronadjiSrednjiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double pronadjiKupovniKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double pronadjiProdajniKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
